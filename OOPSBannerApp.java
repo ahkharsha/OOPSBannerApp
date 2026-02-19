325d9bb36e97c23f81c4046e361b98a55bbfda55
ahkharsha <ahkharsha@gmail.com>
2026-02-19 15:12:47 +0530

    Coded UC2 to print OOPS banner using string concatenation

---
+++ OOPSBannerApp.java
@@ -0,0 +1,23 @@
+/**
+ * OOPSBannerApp UC2 – OOPS Banner Display Application
+ * 
+ * This application prints the OOPS banner by combining text to display it in 7 lines with a width of 9.
+ * 
+ * @author Developer
+ * @version 2.0
+ */
+
+class OOPSBannerApp {
+    // Main method to run the application
+	public static void main(String args[]) {
+
+        // Print the OOPS banner line by line using string concatenation
+		System.out.println("  *****  "+" "+"  *****  "+" "+"******** "+" "+"  ****** ");
+		System.out.println(" **   ** "+" "+" **   ** "+" "+"**     **"+" "+" **    **");
+		System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "+"**       ");
+		System.out.println("**     **"+" "+"**     **"+" "+"******** "+" "+" ******* ");
+		System.out.println("**     **"+" "+"**     **"+" "+"**       "+" "+"       **");
+		System.out.println(" **   ** "+" "+" **   ** "+" "+"**       "+" "+"**    ** ");
+		System.out.println("  *****  "+" "+"  *****  "+" "+"**       "+" "+" ******  ");
+	}
+}
